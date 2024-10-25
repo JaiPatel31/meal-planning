76d80dc61fa8ba4bde6daa534987506c52ba5578
JaiPatel31 <149627486+JaiPatel31@users.noreply.github.com>
2024-10-25 18:07:25 -0500

    Added a loging system just need css

--- meal-planning-backend/server.js
+++ meal-planning-backend/server.js
@@ -1,11 +1,21 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
+
+mongoose.connect("mongodb+srv://jaipatel4717:<bxmqtEaDUtLmRgue>@mealplanningusers.sapy2.mongodb.net/?retryWrites=true&w=majority&appName=MealPlanningUsers", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("Error connecting to MongoDB:", err));
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Meal Planning API!');
@@ -18,6 +28,8 @@ app.post('/api/goals', (req, res) => {
   // For now, send back a placeholder response.
   res.json({ message: 'Goals received', goals: req.body });
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
