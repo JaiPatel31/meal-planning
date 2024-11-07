6eb88cb1c417df7962ad16d74ea3528d2202e8d1
JaiPatel31 <149627486+JaiPatel31@users.noreply.github.com>
2024-11-07 12:11:42 -0600

    added image generation using hugginface api and black forest model

--- meal-planning-backend/server.js
+++ meal-planning-backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,7 +5,13 @@ const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
 const recipeRoutes = require("./routes/recipes");
 const mealPlansRoutes = require("./routes/mealplans");
-mongoose.connect("mongodb+srv://jaipatel4717:bxmqtEaDUtLmRgue@mealplanningusers.sapy2.mongodb.net/?retryWrites=true&w=majority&appName=MealPlanningUsers", { useNewUrlParser: true, useUnifiedTopology: true })
+const imageRoutes = require("./routes/imageRoutes"); // Import the image routes
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://jaipatel4717:bxmqtEaDUtLmRgue@mealplanningusers.sapy2.mongodb.net/?retryWrites=true&w=majority&appName=MealPlanningUsers", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log("Error connecting to MongoDB:", err));
 
@@ -15,17 +20,18 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-
+// Define routes
 app.use("/api/auth", authRoutes);
 app.use("/api/recipes", recipeRoutes);
-app.use('/api/mealplans', mealPlansRoutes);
+app.use("/api/mealplans", mealPlansRoutes);
+app.use("/api/images", imageRoutes); // Add the image route
+
+// Default route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Meal Planning API!');
-  });
+  res.send('Welcome to the Meal Planning API!');
+});
 
-
-
-
+// Start the server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
